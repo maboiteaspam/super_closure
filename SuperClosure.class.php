9b7290e04e2e8a8c4b835f1c1445b0f33ce1c58e
Clement <maboiteaspam@wanadoo.fr>
2011-07-08 13:41:28 +0200

    Added closure serialization within a closure

--- SuperClosure.class.php
+++ SuperClosure.class.php
@@ -14,35 +14,44 @@ class SuperClosure {
 	protected $reflection = NULL;
 	protected $code = NULL;
 	protected $used_variables = array();
+	protected $serialized_variables = array();
 
-
+    /**
+     * @throws InvalidArgumentException
+     * @param $function
+     */
 	public function __construct($function)
 	{
-		// Check if parameter is an actual closure
 		if ( ! $function instanceOf Closure)
 			throw new InvalidArgumentException();
 
-		// Set the member variable values
 		$this->closure = $function;
 		$this->reflection = new ReflectionFunction($function);
 		$this->code = $this->_fetchCode();
 		$this->used_variables = $this->_fetchUsedVariables();
 	}
 
-
+    /**
+     * @return mixed
+     */
 	public function __invoke()
 	{
 		$args = func_get_args();
 		return $this->reflection->invokeArgs($args);
 	}
 
-
+    /**
+     * @return null|function|closure
+     */
 	public function getClosure()
 	{
 		return $this->closure;
 	}
 
-
+    /**
+     * Parse source file to get declarative function code
+     * @return array|string
+     */
 	protected function _fetchCode()
 	{
 		// Open file and seek to the first line of the closure
@@ -65,19 +74,28 @@ class SuperClosure {
 		return $code;
 	}
 
-
+    /**
+     * @return array|null|string
+     */
 	public function getCode()
 	{
 		return $this->code;
 	}
 
-
+    /**
+     * @return array
+     */
 	public function getParameters()
 	{
 		return $this->reflection->getParameters();
 	}
 
-
+    /**
+     * Parse source code to get used variables
+     * within declarative function code
+     *
+     * @return array
+     */
 	protected function _fetchUsedVariables()
 	{
 		// Make sure the use construct is actually used
@@ -92,48 +110,66 @@ class SuperClosure {
 
 		// Get the static variables of the function via reflection
 		$static_vars = $this->reflection->getStaticVariables();
-		
+
 		// Only keep the variables that appeared in both sets
 		$used_vars = array();
 		foreach ($vars as $var)
 		{
-			$var = trim($var, ' $&');
-			$used_vars[$var] = $static_vars[$var];
+			$var = trim($var, ' $');
+            if( $static_vars[$var] instanceOf Closure ){
+                $used_vars[$var] = serialize( new SuperClosure( $static_vars[$var] ) );
+                $this->serialized_variables[] = $var;
+            }else{
+                $used_vars[$var] = $static_vars[$var];
+            }
 		}
 
 		return $used_vars;
 	}
 
-
+    /**
+     * @return array
+     */
 	public function getUsedVariables()
 	{
 		return $this->used_variables;
 	}
 
-
+    /**
+     * Used to correctly serialize object
+     * @return array
+     */
 	public function __sleep()
 	{
-		// Only serialize the code and used_variables, the closure or reflection members with cause a fatal error
-		return array('code', 'used_variables');
+		return array('code', 'used_variables', 'serialized_variables');
 	}
 
-
+    /**
+     * Used to rebuild object
+     *
+     * @throws Exception
+     * @return void
+     */
 	public function __wakeup()
 	{
-		// Import the used variables so they can again be inherited by the closure
-		extract($this->used_variables);
+        foreach( $this->used_variables as $used_var_name=>$used_var ){
+            if( in_array($used_var_name, $this->serialized_variables) ){
+                $used_var = unserialize($used_var);
+            }
+            if( $used_var instanceof SuperClosure ){
+                $$used_var_name = $used_var->closure;
+            }else{
+                $$used_var_name = $used_var;
+            }
+        }
 
-		// Eval the closure's code to recreate the closure
 		eval('$_function = '.$this->code.';');
-
-		// If the eval succeeded create the reflection object as well
 		if (isset($_function) AND $_function instanceOf Closure)
 		{
-			$this->closure = $_function;
-			$this->reflection = new ReflectionFunction($_function);
+			$this->closure      = $_function;
+			$this->reflection   = new ReflectionFunction($_function);
 		}
 		else
-			throw new Exception();
+			throw new Exception("Not a function, or closure");
 	}
-
 }
